ca7ec6a6e94e3bfed0863a76d8fec16c76261517
wu <timmy61501@gmail.com>
2023-10-02 11:17:12 +0800

    php- switch-case

--- if.php
+++ if.php
@@ -1,13 +1,36 @@
 <?php
 
-$score=55;
+$score = 55;
 
 echo "我的成績:" . $score;
 echo "<br>";
 echo "判斷為:";
+echo "<br>";
 
-if($score>=60){
+if ($score >= 60) {
     echo "及格";
-}else{
-    echo"不及格";
+} else {
+    echo "不及格";
 }
+
+$level ="A";
+
+echo "我的等級:" . $level;
+echo "<br>";
+
+switch ($level) {
+    case "A":
+        echo "表現優良";
+        break;
+    case "B":
+        echo "值得肯定";
+        break;
+    case "C":
+        echo "要再加油";
+        break;
+    case "D":
+        echo "需要加強";
+        break;
+}
+
+
