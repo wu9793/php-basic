631fb0db50c8558a684b9a734a24159e1079ac18
wu <timmy61501@gmail.com>
2023-10-02 10:32:28 +0800

    php基礎

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $age=40;
+    echo 40;
+    $name='吳立庭';
+    echo $name;
+
+    $age=$name;
+    $name=$age;
+    $tmp=$age;
+    echo $age;
+    echo $name;
+
+    
+    
+    ?>
+</body>
+
+</html>
