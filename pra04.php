8993523fe6adcaef552b23abf3e1cb70c2cef973
wu <timmy61501@gmail.com>
2023-10-13 15:26:24 +0800

    矩形

--- pra04.php
+++ pra04.php
@@ -127,3 +127,21 @@ for($i=0;$i<9;$i++){
 
 
 ?>
+
+<h2>矩形</h2>
+<?php
+
+for($i=0;$i<7;$i++){
+
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6){
+        echo "*";
+        }else if($j==0 || $j==6){
+            echo "*";
+        }else{
+            echo "&nbsp;";
+        }
+    
+    }
+    echo "<br>";
+}
