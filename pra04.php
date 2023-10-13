44e87f3e020f2fbb5cffc1a1cb9b899dbbbf2026
wu <timmy61501@gmail.com>
2023-10-13 14:04:00 +0800

    正三角形

--- pra04.php
+++ pra04.php
@@ -61,6 +61,26 @@ for($i=4;$i>=0;$i--){ //行列數
 }
 echo "<br>";
 }
+?>
+
+<style>
+    *{
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
+
+<h2>正三角形</h2>
+<?php
+for($i=0;$i<5;$i++){ 
+    for($j=0;$j<(4-$i);$j++){
+        echo "&nbsp;";
+    }    
+    for($k=0;$k<($i*2+1);$k++){
+          echo "*";  
+    }
+     
+echo "<br>";
+}
 
 
 ?>
