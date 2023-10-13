79d2bb1c20e39f37e05945398bccb87074bb5679
wu <timmy61501@gmail.com>
2023-10-13 13:25:27 +0800

    直角三角形

---
+++ pra04.php
@@ -0,0 +1,53 @@
+<h2>直角三角形</h2>
+<?php
+
+echo "*<br>";
+echo "**<br>";
+echo "***<br>";
+echo "****<br>";
+echo "*****<br>";
+echo "<hr>";
+
+/* echo "*<br>";
+    echo "*";
+    echo "<br>";
+echo "**<br>";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "***<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "****<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "*****<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "<hr>";
+$i  $j
+0 < 1
+1   2
+2   3
+3   4
+4   5
+ */
+
+for($i=0;$i<5;$i++){
+    for($j=0;$j<=$i;$j++){
+    echo "*";
+}
+echo "<br>";
+}
+?>
+
+
