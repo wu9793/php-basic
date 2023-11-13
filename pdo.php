f0f10619fd6f191d2e1096f3e6dc113bea21b624
wu <timmy61501@gmail.com>
2023-11-13 11:18:50 +0800

    exec

--- pdo.php
+++ pdo.php
@@ -18,5 +18,16 @@ $pdo=new PDO($dsn,'root','');
 // $sql="delete from `dept` where `id`='8'";
 // $pdo->query($sql);
 
-$sql="delete from `dept` where `id`='7'";
-$pdo->query($sql);
+// $sql="delete from `dept` where `id`='7'";
+// $pdo->query($sql);
+
+// exec 和 query 一樣可以顯示執行結果
+// $sql="delete from `dept` where `id`='7'";
+// echo $pdo->exec($sql);
+
+
+// $sql="insert into `dept`(`code`,`name`)values('701','演藝科');";
+// echo $pdo->exec($sql);
+
+$sql="insert into `dept`(`code`,`name`)values('801','汽修科');";
+echo $pdo->exec($sql);
